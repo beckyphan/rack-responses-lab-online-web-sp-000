2d807b7349155e73f60123091806d8a0ff70e0a5
Becky Phan <beckyphan@live.com>
2019-08-03 17:40:13 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -3,7 +3,7 @@ class Application
   def call(env)
     resp = Rack::Response.new
  
-    current_time = Time.now.to_s
+    current_time = Time.now
     time = current_time.hour
     resp.write "#{time}"
  
